c145cd196fd390f6facee73f803ca9aeeed54d47
Lenni <gamer_like_you@gmx.de>
2025-12-11 00:22:04 +0100

    first code, yay

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{client, Result};
+
+// mache erstmal die docs durch also noch nicht direkt ein messenger
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // connecting to the redis
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // set a key named "hello" to value "world"
+    client.set("hello", "world".into()).await?;
+
+    // get the value of the key "hello again"
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
 }
